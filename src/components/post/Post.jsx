f6842bd606abbbe01153b6c7762e51efc7999e6f
OliWebDevO <oliver-vdb@hotmail.com>
2024-06-01 19:27:00 +0200

    post component created and shadow in css

---
+++ src/components/post/Post.jsx
@@ -0,0 +1,45 @@
+import './post.scss'
+import React from 'react';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import CommentIcon from '@mui/icons-material/Comment';
+import ShareIcon from '@mui/icons-material/Share';
+
+const Post = ({post}) => {
+    return (
+        <div className="post">
+            <div className="post-head">
+                <img className='user-img' src={post.profilePic} alt="" />
+                <div className="user-infos">
+                    <div className="user-info">
+                        <p>{post.name}</p>
+                        <p className='small'>a few seconds ago</p>
+                    </div>
+                    <div className="user-infos-icon">
+                        <MoreHorizIcon className='icon'/>
+                    </div>
+                </div>
+            </div>
+            <div className="post-content">
+                <p>{post.desc}</p>
+                <img src={post.img} alt="" />
+                <div className="post-reactions">
+                    <div className="post-like">
+                        <FavoriteBorderIcon className='icon'/>
+                        <a href="">Like</a>
+                    </div>
+                    <div className="post-like">
+                        <CommentIcon className='icon'/>
+                        <a href="">Comment</a>
+                    </div>
+                    <div className="post-like">
+                        <ShareIcon className='icon'/>
+                        <a href="">Share</a>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Post
--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -1,8 +1,5 @@
 import './posts.scss'
-import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
-import CommentIcon from '@mui/icons-material/Comment';
-import ShareIcon from '@mui/icons-material/Share';
+import Post from '../post/Post';
 
 const Posts = () => {
     const posts = [
@@ -50,39 +47,7 @@ const Posts = () => {
     return (
         <div className="posts">
             {posts.map(post => (
-                <div className="post">
-                    <div className="post-head">
-                        <img className='user-img' src={post.profilePic} alt="" />
-                        <div className="user-infos">
-                            <div className="user-info">
-                                <p>{post.name}</p>
-                                <p className='small'>a few seconds ago</p>
-                            </div>
-                            <div className="user-infos-icon">
-                                <MoreHorizIcon className='icon'/>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="post-content">
-                        <p>{post.desc}</p>
-                        <img src={post.img} alt="" />
-                        <div className="post-reactions">
-                            <div className="post-like">
-                                <FavoriteBorderIcon className='icon'/>
-                                <a href="">Like</a>
-                            </div>
-                            <div className="post-like">
-                                <CommentIcon className='icon'/>
-                                <a href="">Comment</a>
-                            </div>
-                            <div className="post-like">
-                                <ShareIcon className='icon'/>
-                                <a href="">Share</a>
-                            </div>
-
-                        </div>
-                    </div>
-                </div>
+               <Post post={post} key ={post.id}/>
             ))}
         </div>
     )
